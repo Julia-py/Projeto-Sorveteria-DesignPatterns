009b8bb6901a2d4be23cacc795fee3a55507e088
rose-del <rosenildasantos.estudante@gmail.com>
2025-06-19 11:12:39 -0300

    feat: Deixando apenas um Main e apagando a pasta .idea

--- GelatoMix/src/main/java/gelatomix/controller/Main.java
+++ GelatoMix/src/main/java/gelatomix/controller/Main.java
@@ -1,21 +1,36 @@
 package gelatomix.controller;
 
+import gelatomix.model.Pedido;
+import gelatomix.model.decorator.Brownie;
 import gelatomix.model.decorator.LeiteNinho;
 import gelatomix.model.facade.GelatomixFacade;
 import gelatomix.model.factory.FactoryBuilder;
+import gelatomix.model.factory.FactoryMassaChocolate;
 import gelatomix.model.factory.FactoryMilkshakeChocolate;
 import gelatomix.model.factory.FactoryMilkshakeMenta;
+import gelatomix.model.factory.sorvetes.MassaChocolate;
+import gelatomix.model.factory.sorvetes.MilkshakeChocolate;
 import gelatomix.model.interfaces.Sorvetes;
+import gelatomix.model.singleton.FiladePedidos;
 
 
 public class Main {
     public static void main(String[] args) {
-        //Sorvetes milkShake = FactoryBuilder.criarSorvete(new FactoryMilkshakeMenta());
-        //System.out.println(milkShake.getDescricao());
-        //System.out.println(milkShake.getPreco());
+        //decorator
+        Sorvetes milkshake = new MilkshakeChocolate();
+        milkshake = new LeiteNinho(milkshake);
+        System.out.println(milkshake.getDescricao());
+        System.out.println(milkshake.getPreco());
 
+        Sorvetes casquinhaChocolate = new MassaChocolate();
+        casquinhaChocolate = new Brownie(casquinhaChocolate);
+        System.out.println(casquinhaChocolate.getDescricao());
+        System.out.println(casquinhaChocolate.getPreco());
+
+        //facade
         GelatomixFacade facade = new GelatomixFacade();
-
         facade.fazerPedido(new FactoryMilkshakeChocolate());
+
+        facade.fazerPedido(new FactoryMassaChocolate());
     }
 }
